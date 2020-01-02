17d0bf8712fec21e1e58638acfe365e54d26797d
amitmichael <amitmichael22@gmail.com>
2020-01-02 21:28:00 +0200

    1

---
+++ src/main/java/bgu/spl/net/api/StompMessagingProtocolImpl.java
@@ -0,0 +1,41 @@
+package bgu.spl.net.api;
+import bgu.spl.net.messagebroker.Client;
+import bgu.spl.net.messagebroker.MessageBroker;
+import bgu.spl.net.srv.Connections;
+import java.io.Serializable;
+import java.util.function.Supplier;
+
+public class StompMessagingProtocolImpl<T> implements StompMessagingProtocol<Serializable>, Supplier<MessagingProtocol<T>> {
+    private MessageBroker messageBroker;
+    private Client client;
+    private int connectionid;
+    private Connections connections;
+    private boolean terminate;
+
+    public StompMessagingProtocolImpl(MessageBroker a){
+        this.messageBroker = a;
+    }
+
+    public void start(int connectionId, Connections<String> connections) {
+        this.connectionid = connectionId;
+        this.connections = connections;
+    }
+
+    @Override
+    public void process(String message) {
+
+    }
+
+    @Override
+    public boolean shouldTerminate() {
+        if (this.terminate ==true){
+            connections.disconnect(connectionid);
+        }
+        return this.terminate;
+    }
+
+    @Override
+    public MessagingProtocol<T> get() {
+        return null;
+    }
+}
---
+++ src/main/java/bgu/spl/net/srv/LogManager.java
@@ -0,0 +1,44 @@
+package bgu.spl.net.srv;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LogManager {
+    public Logger log;
+    private static class singletonHolder{
+        private static LogManager logInstance = new LogManager("Ass3");}
+    FileHandler fh;
+
+    private LogManager(String path){
+        log = Logger.getLogger("");
+
+        try {
+
+            // This block configure the logger with handler and formatter
+            try {
+                File file = new File (path);
+                fh = new FileHandler(path);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            log.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            log.setUseParentHandlers(false);
+
+            // the following statement is used to log any messages
+            log.info("log start");
+
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static LogManager getInstance() {
+        return singletonHolder.logInstance;
+    }
+}
