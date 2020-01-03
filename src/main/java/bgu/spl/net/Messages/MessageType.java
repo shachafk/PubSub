b53b4eb74722c0b6199f767e9fb148775a43abb5
shachafk <katz.shachaf@gmail.com>
2020-01-03 14:50:51 +0200

    1

---
+++ src/main/java/bgu/spl/net/Messages/MessageType.java
@@ -0,0 +1,16 @@
+package bgu.spl.net.Messages;
+
+public class MessageType {
+
+     static enum type{
+         Login,
+         JoinGenre,
+         ExitGenre,
+         AddBook,
+         BorrowBook,
+         ReturnBook,
+         GenreBookStatus,
+         Logout
+    }
+
+}
