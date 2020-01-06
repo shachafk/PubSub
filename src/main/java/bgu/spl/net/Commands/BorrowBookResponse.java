3872fd0fdc010adfd7c5207122a6888afa0373e8
amitmichael <amitmichael22@gmail.com>
2020-01-06 19:17:49 +0200

    8

---
+++ src/main/java/bgu/spl/net/Commands/BorrowBookResponse.java
@@ -0,0 +1,50 @@
+package bgu.spl.net.Commands;
+
+import bgu.spl.net.PassiveObjects.User;
+import bgu.spl.net.api.Message;
+import bgu.spl.net.impl.rci.Command;
+import bgu.spl.net.srv.LogManager;
+
+import java.io.Serializable;
+
+public class BorrowBookResponse implements Command {
+    private LogManager logM = LogManager.getInstance();
+    private CommandType type = CommandType.BorrowBookResponse;
+    private String genre;
+    private String bookName;
+    private String loaner;
+    private String body;
+
+    public BorrowBookResponse(Message msg){
+        if (!msg.getCommand().equals("SEND") | msg.getHeader().size() < 1) {
+            logM.log.severe("BorrowBookResponse msg is not valid");
+            return;
+        } else {
+            this.genre = msg.getHeader().get(0).getSecond();
+            this.body = msg.getBody();
+            this.loaner=body.substring(0,body.indexOf(" "));
+            this.bookName=body.substring(body.indexOf("has")+4);
+        }
+    }
+
+    @Override
+    public Serializable execute(Object arg) {
+        if (arg instanceof User) {
+            User user = (User) arg;
+            Message toReturn = new Message();
+            toReturn.setCommand("MESSAGE");
+            toReturn.addHeader("destination",""+genre);
+            toReturn.setBody(""+loaner+" has "+bookName);
+            return toReturn;
+        }
+        else{
+            logM.log.severe("arg isnt instance of User");
+            return null;
+        }
+    }
+
+    @Override
+    public CommandType getType() {
+        return type;
+    }
+}
